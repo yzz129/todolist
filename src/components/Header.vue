fba7e4d8ad4896504ed6c396aa45d00042482f74
yzz129 <154991996+yzz129@users.noreply.github.com>
2024-08-06 17:59:30 +0800

    Update Header.vue
    修改搜索栏bug

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,7 +9,8 @@
           type="text"
           class="search-input"
           placeholder="搜索..."
-          v-model="searchQuery"
+          v-model="localSearchQuery"
+          @input="updateSearchQuery(localSearchQuery)"
       />
       <button class="headerAllSelect" @click="handleAllSelect">全选</button>
       <button class="headerAdd" @click="handleAdd">添加</button>
@@ -37,6 +38,21 @@ export default {
       type: Function,
       required: true
     }
+  },
+  data() {
+    return {
+      localSearchQuery: this.searchQuery
+    };
+  },
+  watch: {
+    searchQuery(newVal) {
+      this.localSearchQuery = newVal;
+    }
+  },
+  methods: {
+    updateSearchQuery(newQuery) {
+      this.$emit('update:searchQuery', newQuery);
+    }
   }
 }
 </script>
@@ -46,9 +62,7 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  border-bottom: 1px solid rgba(204, 204, 230, 0.7); /* 更柔和的边框颜色 */
-  border-radius: 10px; /* 边框圆滑度 */
-  padding: 10px 0; /* 添加内边距，使元素看起来更大更圆滑 */
+  border-bottom: #ccc 1px solid;
 
   .header-left {
     display: flex;
@@ -57,48 +71,42 @@ export default {
     div {
       width: 50px;
       height: 50px;
-      border-radius: 50%; /* 圆形按钮 */
+      border-radius: 50%;
       background: #9999E6;
       font-size: 30px;
       text-align: center;
       margin-right: 15px;
-      color: #fff; /* 字体颜色更可爱 */
     }
   }
 
   .header-right {
     .headerAllSelect {
-      background: #46e6e0;
+      background: #27b6c8;
       border-radius: 20px; /* 更圆滑的按钮 */
-      font-size: 16px; /* 字体大小调整，使字体更可爱 */
-      font-family: '宋体', sans-serif;
+      font-family: sans-serif;
     }
 
     .headerAdd {
       background: #70B870;
       border-radius: 20px; /* 更圆滑的按钮 */
-      font-size: 16px;
       font-family: sans-serif;
     }
 
     .headerDelete {
-      background: #C43F38; /* 删除按钮颜色保持不变 */
-      border-radius: 20px; /* 更圆滑的按钮 */
+      background: #C43F38; // 设置删除按钮的样式
       margin-left: 10px;
-      font-size: 16px;
+      border-radius: 20px; /* 更圆滑的按钮 */
       font-family: sans-serif;
-
     }
   }
+}
 
-  .search-input {
-    padding: 0.5px;
-    border: 0.5px solid rgba(204, 204, 230, 0.7); /* 更柔和的边框颜色 */
-    border-radius: 4px; /* 边框圆滑度 */
-    margin-left: 10px;
-    color: rgba(0, 0, 0, 0.98); /* 输入框文字颜色 */
-    font-size: 16px;
-    font-family:  sans-serif;
-  }
+.search-input {
+  padding: 1px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  margin-left: 10px;
+  color: #000; // 输入框文字颜色
+  font-family: sans-serif;
 }
 </style>
